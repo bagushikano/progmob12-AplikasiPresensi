924a5f14ab82b5cbde33b897498af68dd75f221a
bgsw0709 <57007068+bgsw0709@users.noreply.github.com>
2020-10-09 20:49:53 +0800

    Login, Register
    jos ui login, ui register

--- app/src/main/java/com/progmob12/aplikasipresensi/MainActivity.java
+++ app/src/main/java/com/progmob12/aplikasipresensi/MainActivity.java
@@ -2,7 +2,10 @@ package com.progmob12.aplikasipresensi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.progmob12.aplikasipresensi.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent loginActivity = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(loginActivity);
     }
 }
